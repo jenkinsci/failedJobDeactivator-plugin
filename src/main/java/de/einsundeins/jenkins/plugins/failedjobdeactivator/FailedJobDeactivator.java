4265f529bbb9199d62092fd5a5d147dd39b3269d
Jochen Fürbacher <jochen.fuerbacher@1und1.de>
2017-07-04 09:37:50 +0200

    Added optional bfa; show failure causes.

--- src/main/java/de/einsundeins/jenkins/plugins/failedjobdeactivator/FailedJobDeactivator.java
+++ src/main/java/de/einsundeins/jenkins/plugins/failedjobdeactivator/FailedJobDeactivator.java
@@ -36,10 +36,14 @@ import javax.servlet.ServletException;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
+import com.sonyericsson.jenkins.plugins.bfa.model.FailureCauseBuildAction;
+import com.sonyericsson.jenkins.plugins.bfa.model.FoundFailureCause;
+
 import hudson.Plugin;
 import hudson.model.AbstractProject;
 import hudson.model.Item;
 import hudson.model.Job;
+import hudson.model.Run;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
@@ -80,6 +84,42 @@ public class FailedJobDeactivator extends Plugin {
 			}
 		}
 		return false;
+	}
+
+	public boolean isBuildFailureAnalyserAvailable() {
+		Plugin plugin = Jenkins.getInstance()
+				.getPlugin("build-failure-analyzer");
+
+		if (plugin == null)
+			return false;
+
+		return true;
+	}
+
+	public String getFailureCauses(String jobName) {
+		for (Item item : Jenkins.getInstance().getAllItems()) {
+			if (item.getName().equals(jobName)) {
+				Job<?, ?> job = (Job<?, ?>) item;
+				Run<?, ?> build = job.getLastBuild();
+				if (build == null)
+					return null;
+
+				FailureCauseBuildAction action = build
+						.getAction(FailureCauseBuildAction.class);
+				if (action == null)
+					return null;
+
+				String failureCauses = "";
+				for (FoundFailureCause failureCause : action
+						.getFoundFailureCauses()) {
+					failureCauses = failureCauses + failureCause.getName()
+							+ "\n";
+				}
+
+				return failureCauses;
+			}
+		}
+		return null;
 	}
 
 	public void doHandleJobs(StaplerRequest req, StaplerResponse rsp)
